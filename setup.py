e73afecef9399183a303090081f9782addba145d
arthurpinange <arthur.pinange@gmail.com>
2020-04-26 14:21:55 -0300

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pycepbr',
+      versão='1.0',
+      description=' Consulta CEPs de todo território brasileiro',
+      url='https://github.com/Arthurpinange/pycepbr',
+      author='Arthur Pinangé',
+      author_email='arthur.pinange@gmail.com',
+      license='MIT',
+      packages=['pycepbr'],
+      zip_safe=False)
